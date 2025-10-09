d15e00984c38ba12c0b67c2d24af70662b575027
rossv <ross.volkwein@gmail.com>
2025-10-09 09:46:05 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import preprocess from 'svelte-preprocess' 
 
-// NOTE: Set base to "/<repo-name>/" after you create the GitHub repo.
 export default defineConfig({
-  plugins: [svelte()],
-  base: '/designstorms'
+  plugins: [svelte({ preprocess: preprocess() })],
+  base: '/designstorms/'
 })
