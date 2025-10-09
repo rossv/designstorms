0db59206b32e902b44c9239ed02ee6d6c74b7ede
rossv <ross.volkwein@gmail.com>
2025-10-09 16:27:38 -0400

    Use design storm favicon

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,3 +4,13 @@ declare module '*.png' {
   const src: string
   export default src
 }
+
+declare module '*.ico' {
+  const src: string
+  export default src
+}
+
+declare module '*.ico?url' {
+  const src: string
+  export default src
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import App from './App.svelte'
 import 'leaflet/dist/leaflet.css'
 import './theme.css'
+import faviconUrl from './design_storm.ico?url'
+
+function ensureFavicon() {
+  let link = document.querySelector<HTMLLinkElement>("link[rel='icon']")
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  if (link.href !== faviconUrl) {
+    link.href = faviconUrl
+  }
+}
+
+ensureFavicon()
 
 const app = new App({ target: document.getElementById('app')! })
 export default app
