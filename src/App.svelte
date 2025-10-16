e664b23489ffd78e9cf5b235bb943ca9ccf9fe10
rossv <ross.volkwein@gmail.com>
2025-10-16 08:24:47 -0400

    Simplify NOAA table mobile labels

--- src/App.svelte
+++ src/App.svelte
@@ -2647,7 +2647,7 @@
     }
 
     .table-button.cell::before {
-      content: 'Average Recurrence Interval — ' attr(data-ari) ' yr';
+      content: attr(data-ari) ' yr';
       font-size: 11px;
       letter-spacing: 0.06em;
       text-transform: uppercase;
